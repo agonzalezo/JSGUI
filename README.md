b8708bdb7e0a9a56224f9635533f6a98d7460599
Alexander Gonzalez <mexi1415@gmail.com>
2022-08-08 20:22:08 -0500

    mouse and keyboard actions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JSGUI
 Scripts and more to manage the gui
+
+you need python and visual studio c++ to run.
---
+++ src/keyboard.js
@@ -0,0 +1,17 @@
+//#region set a vars
+const robotjs = require("robotjs");
+robotjs.setKeyboardDelay(200);
+
+//#region Open a command line
+    robotjs.keyTap("r", "command")
+    robotjs.setKeyboardDelay(1);
+    robotjs.typeString('"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"');
+    robotjs.setKeyboardDelay(1000);
+    robotjs.keyTap("enter");
+    robotjs.setKeyboardDelay(500);
+    robotjs.keyTap("n",["control", "shift"]);
+    robotjs.typeString("Stiven gay fotos");
+    robotjs.keyTap("enter");
+    robotjs.setMouseDelay(200)
+    robotjs.moveMouse(235,209);
+    robotjs.mouseClick();
